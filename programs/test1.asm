50f61237d58838927b7ca513760a17501a48b441
Jan Meznik <jan@meznik.dk>
2016-03-01 15:07:31 +0100

    Modified test1.asm.

--- programs/test1.asm
+++ programs/test1.asm
@@ -1,3 +1,9 @@
-add $2, $0, $0
-add $8, $9, $10
-add $9, $11, $8
+.globl _start
+_start:
+	addi $t0, $0, 10	# $t0 = 10
+	add $t1, $t0, $t0	# $t1 = 20
+	add $t2, $t1, $t0	# $t2 = 30
+	add $t1, $t2, $t1	# $t1 = 50
+	nop
+	nop
+	nop
