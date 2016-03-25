c0234eaca1bb9f6391051604016ad62a7bc47634
Jan Meznik <jan@meznik.dk>
2016-03-25 17:43:08 +0100

    Added LOG() debug macro.

--- src/error.h
+++ src/error.h
@@ -5,10 +5,14 @@
 #include <errno.h>
 #include <string.h>
 
+#define LOG(str, ...) fprintf(stdout, "[LOG] %s:%d: " str "\n", \
+			      __FILE__, __LINE__, ##__VA_ARGS__)
+
 #define ERROR(str, ...) fprintf(stderr, "[ERROR] %s:%d: " str "\n", \
 				__FILE__, __LINE__, ##__VA_ARGS__)
 
 #define DEBUG(str, ...) fprintf(stdout, "[DEBUG] %s:%d: " str "\n", \
 				__FILE__, __LINE__, #__VA_ARGS__)
+
 
 #endif /* _ERROR_H */
