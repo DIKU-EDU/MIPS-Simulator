6de059fd2598078eec5e7b4ccb8eda9649b8b13c
Jan Meznik <jan@meznik.dk>
2016-03-25 13:46:44 +0100

    Added ERROR and DEBUG macro.

---
+++ src/error.h
@@ -0,0 +1,14 @@
+#ifndef _ERROR_H
+#define _ERROR_H
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#define ERROR(str, ...) fprintf(stderr, "[ERROR] %s:%d: " str "\n", \
+				__FILE__, __LINE__, ##__VA_ARGS__)
+
+#define DEBUG(str, ...) fprintf(stdout, "[DEBUG] %s:%d: " str "\n", \
+				__FILE__, __LINE__, #__VA_ARGS__)
+
+#endif /* _ERROR_H */
