233f7c166b245c6ecad2a093bf987fc1acdf10a8
Jan Meznik <jan@meznik.dk>
2016-04-12 17:25:45 +0200

    Added Exception Code definitions in exception.h

--- src/exception.h
+++ src/exception.h
@@ -2,5 +2,24 @@
 #define _EXCEPTION_H
 
 
+/* Exception codes
+ * COD Green Card */
+typedef enum exceptions {
+	EXC_Interrupt,
+	EXC_AddressErrorLoad,
+	EXC_AddressErrorStore,
+	EXC_BusErrorInstructionFetch,
+	EXC_BusErrorLoadStore,
+	EXC_Syscall,
+	EXC_Breakpoint,
+	EXC_Reserved,
+	EXC_CpUnavailable,
+	EXC_ArithmeticOverflow,
+	EXC_Trap,
+	EXC_FloatingPoint
+} exceptions_t;
+
+
+void raise_exception(uint32_t exception_code, core_t *core);
 
 #endif /* _EXCEPTION_H */
