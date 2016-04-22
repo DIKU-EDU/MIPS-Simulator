7494f13cb238ec0977578ac6871cf959877153d7
Jan Meznik <jan@meznik.dk>
2016-04-22 10:30:03 +0200

    Exception definitions.

--- src/exception.c
+++ src/exception.c
@@ -6,3 +6,21 @@ void raise_exception(uint32_t exception_code, core_t *core)
 {
 	return;
 }
+
+/* Array of register names */
+char *exc_names [] = {
+	"None",
+	"Interrupt",
+	"AddressErrorLoad",
+	"AddressErrorStore",
+	"BusErrorInstructionFetch",
+	"BusErrorLoadStore",
+	"Syscall",
+	"Breakpoint",
+	"Reserved",
+	"CpUnavailable",
+	"ArithmeticOverflow",
+	"Trap",
+	"FloatingPoint",
+	NULL
+};
--- src/exception.h
+++ src/exception.h
@@ -3,6 +3,9 @@
 
 #include "cpu.h"
 
+#define NUM_EXCEPTIONS 16
+extern char *exc_names[NUM_EXCEPTIONS];	/* Defined in exception.c */
+
 void raise_exception(uint32_t exception_code, core_t *core);
 
 #endif /* _EXCEPTION_H */
