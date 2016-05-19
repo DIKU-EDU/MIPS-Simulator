cd23c08ef8d5154ea327ade206a96ecadc3fe0d6
Jan Meznik <jan@meznik.dk>
2016-05-19 12:22:18 +0200

    Good golly! Mixup between shutdown_dev.c and shutdown_dev.h.

--- src/shutdown_dev.c
+++ src/shutdown_dev.c
@@ -1,17 +1,26 @@
-#ifndef _SHUTDOWN_DEV_H
-#define _SHUTDOWN_DEV_H
-
-#include <stdint.h>
-
 #include "io.h"
+#include "shutdown_dev.h"
 
-/* Size of device registers */
-#define IO_LENGTH_SHUTDOWN	0x04
-#define POWEROFF_SHUTDOWN_MAGIC 0x0badf00d /* Defined in KUDOS */
+int shutdown_device_init(device_descriptor_t *dev)
+{
+	dev->device_type = TYPECODE_SHUTDOWN;
+	memcpy(dev->vendor_string, "SHUTDOWN", 8);
+	dev->irq = IRQ_INVALID;
+	dev->io_write = &shutdown_device_write;
 
-io_device_descriptor_t *shutdown_device_init();
+	dev->io_addr_len = IO_LENGTH_SHUTDOWN;
+
+	return dev;
+}
 
 int shutdown_device_write(io_device_descriptor_t *dev, uint32_t addr,
-			  uint32_t data);
+			  uint32_t data)
+{
+	if(data == POWEROFF_SHUTDOWN_MAGIC) {
+		g_finished = true;
+	}
+	return 0;
+}
 
-#endif /* _SHUTDOWN_DEV_H */
+
+
--- src/shutdown_dev.h
+++ src/shutdown_dev.h
@@ -1,27 +1,19 @@
+#ifndef _SHUTDOWN_DEV_H
+#define _SHUTDOWN_DEV_H
+
+#include <stdint.h>
+
 #include "io.h"
-#include "shutdown_dev.h"
 
-int shutdown_device_init(device_descriptor_t *dev)
-{
-	dev->device_type = TYPECODE_SHUTDOWN;
-	memcpy(dev->vendor_string, "SHUTDOWN", 8);
-	dev->irq = IRQ_INVALID;
-	dev->io_write = &shutdown_device_write;
+/* Size of device registers */
+#define IO_LENGTH_SHUTDOWN	0x04
+#define POWEROFF_SHUTDOWN_MAGIC 0x0badf00d /* Defined in KUDOS */
 
-	dev->io_addr_len =
-
-
-	return dev;
-}
+io_device_descriptor_t *shutdown_device_init();
 
 int shutdown_device_write(io_device_descriptor_t *dev, uint32_t addr,
-			  uint32_t data)
-{
-	if(data == POWEROFF_SHUTDOWN_MAGIC) {
-		g_finished = true;
-	}
-	return 0;
-}
+			  uint32_t data);
 
+#endif /* _SHUTDOWN_DEV_H */
 
 
